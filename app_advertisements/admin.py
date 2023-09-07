bd18592f9acb1127923d945c447971e982b492c6
Fedor <Fedor-Berest@mail.ru>
2023-09-07 15:56:08 +0300

    Урок 9 Связь model и template, и медиа-файлы

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Advertisement
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'created_date', 'auction']
+    list_display = ['id', 'user', 'title', 'description', 'created_date', 'update_date', 'auction', 'image']
     list_filter = ['created_at', 'auction']
 
 admin.site.register(Advertisement, AdvertisementAdmin)
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib import admin
 from django.utils import timezone
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 # Create your models here.
 
 class Advertisement(models.Model):
@@ -11,6 +14,9 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Укажите уместен ли торг')
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь',
+                             on_delete=models.CASCADE)
+    image = models.ImageField('Изображение', upload_to='advertisements/')
 
     def __str__(self):
         return f'<Advertisement: Advertisement(id={self.id}title={self.title}, price={self.price})>'
@@ -20,10 +26,20 @@ class Advertisement(models.Model):
         if self.created_at.date() == timezone.now().date():
             created_time = self.created_at.time().strftime("%H:%M:%S")
             return format_html(
-                '<span style = "color: red">Сегодня в {}</span>', created_time
+                '<span style = "color: green">Сегодня в {}</span>', created_time
             )
         else:
             return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Дата обнавления')
+    def update_date(self):
+        if self.update_at.date() == timezone.now().date():
+            update_time = self.update_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style = "color: red">Сегодня в {}</span>', update_time
+            )
+        else:
+            return self.update_at.strftime("%d.%m.%Y в %H:%M:%S")
 
 
     class Meta:
